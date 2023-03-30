5d5ea364276c5df7117a28e788dd7ddbc49e5a69
Mayurpatil1234 <mayurpatil7456@gmail.com>
2023-03-30 16:57:45 +0530

    ayush clone commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './global.css';
+import About from './pages/about/about';
+import about from './pages/about/about.css';
+
+function App() {
+  return (
+    
+        <About/>
+        
+    
+  );
+}
+
+export default App;
